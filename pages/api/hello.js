a047b4ac23b7b0c32249423574ac37ba890b8bd9
mandragora-hub <royer01gomez@gmail.comn>
2023-08-26 18:47:11 -0400

    🤸‍♀ Update hello function

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,19 +1,21 @@
 import { loadDocument } from "pdf-metadata";
 
 export default async function handler(req, res) {
-  try {
-    const { fileUrl } = req.body;
-    if (!fileUrl) throw new Error("Must send a valid fileUrl.");
+  if (req.method === "POST") {
+    try {
+      const { fileUrl } = req.body;
+      if (!fileUrl) throw new Error("Must send a valid fileUrl.");
 
-    console.log(`Processing ${fileUrl}.`);
+      console.log(`Processing ${fileUrl}.`);
 
-    const document = await loadDocument(fileUrl);
-    const info = await document.getInfo();
+      const document = await loadDocument(fileUrl);
+      const info = await document.getInfo();
 
-    return res.status(200).json(info);
-  } catch (error) {
-    return res.status(400).json(error);
+      return res.status(200).json(info);
+    } catch (error) {
+      return res.status(400).json(error);
+    }
+  } else {
+    res.status(400).send("Http method must be POST");
   }
-
-  // res.status(200).json({ message: 'Hello from Next.js!' })
 }
