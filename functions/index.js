e95be398d43504b3ab01f5da552d49627d9adba2
mandragora-hub <royer01gomez@gmail.comn>
2023-08-26 10:10:44 -0400

    🤸‍♀ convert to anonymous function

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,11 @@
-import { loadDocument } from 'pdf-metadata';
+/* eslint-disable import/no-anonymous-default-export */
+import { loadDocument } from "pdf-metadata";
 
-const metadataExtractor = async (req, res) => {
+export default async (req, res) => {
   const { fileUrl } = req.body;
-  
+
   const document = await loadDocument(fileUrl);
   const info = await document.getInfo();
 
   res.status(200).json(info);
 };
-
-export default metadataExtractor;
