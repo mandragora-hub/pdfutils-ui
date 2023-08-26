5322fc0d649c4cc74c2ec63decb880926e7ffa7c
mandragora-hub <royer01gomez@gmail.comn>
2023-08-26 18:25:55 -0400

    🤸‍♀ update hello function

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,19 @@
-export default function handler(req, res) {
-    res.status(200).json({ message: 'Hello from Next.js!' })
+import { loadDocument } from "pdf-metadata";
+
+export default async function handler(req, res) {
+  try {
+    const { fileUrl } = req.body;
+    if (!fileUrl) throw new Error("Must send a valid fileUrl.");
+
+    console.log(`Processing ${fileUrl}.`);
+
+    const document = await loadDocument(fileUrl);
+    const info = await document.getInfo();
+
+    return res.status(200).json(info);
+  } catch (error) {
+    return res.status(400).json(error);
   }
+
+  // res.status(200).json({ message: 'Hello from Next.js!' })
+}
