ff153c1d0a3ba9e2bd57c952f2c7bf4720b61134
mandragora-hub <royer01gomez@gmail.comn>
2023-08-26 10:39:03 -0400

    ⏫ Update again

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,17 @@
 /* eslint-disable import/no-anonymous-default-export */
-// import { loadDocument } from "pdf-metadata";
+import { loadDocument } from "pdf-metadata";
 
 export default async (req, res) => {
   try {
     const { fileUrl } = req.body;
-    if (!fileUrl) throw new Error("Must send a valid  fileUrl.");
-    // const document = await loadDocument("https://litterarum.onrender.com/api/v1/files/el-hombre-que-calculaba-malba-tahan.pdf");
-    // const info = await document.getInfo();
-    res.status(200).json({ info: "fileUrl" });
+    if (!fileUrl) throw new Error("Must send a valid fileUrl.");
+
+    console.log(`Processing ${fileUrl}.`);
+
+    const document = await loadDocument(fileUrl);
+    const info = await document.getInfo();
+    return res.status(200).json(info);
   } catch (error) {
-    return res.status(400).json({ error });
+    return res.status(400).json(error);
   }
 };
