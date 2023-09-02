bb9dc30fb8a56ca8ed77bab5d95ba02df86bd175
mandragora-hub <royer01gomez@gmail.comn>
2023-09-02 13:12:45 -0400

    🗑 Remove plausible

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,7 @@
 import "../styles/globals.css";
-import PlausibleProvider from "next-plausible";
-import getConfig from "next/config";
 
 function MyApp({ Component, pageProps }) {
-  const { publicRuntimeConfig } = getConfig();
-
-  return (
-    <PlausibleProvider
-      selfHosted
-      enabled
-      domain={publicRuntimeConfig.plausibleDomain}
-      customDomain={publicRuntimeConfig.plausibleCustomDomain}
-    >
-      <Component {...pageProps} />
-    </PlausibleProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
